024b574336e25416d220cc04fc23f57be6960dac
StevenS79 <74082325+StevenS79@users.noreply.github.com>
2020-12-26 23:01:35 -0500

    Add files via upload

---
+++ C35 Project.js
@@ -0,0 +1,38 @@
+//Create variables here
+
+function preload()
+{
+	//load images here
+}
+
+function setup() {
+	createCanvas(800, 700);
+  background(46, 139, 87);
+}
+
+
+function draw() {  
+
+if (keyWentDown(UP_ARROW)){
+  writestock(9foodS);
+  DOMStringList.addImage(dogHappy);
+}
+
+  drawSprites();
+  //add styles here
+
+  text ("Press Up Arrow Key To Feed Drago Milk!");
+  textSize(20);
+
+
+}
+
+function readStock(data){
+  foodS=data.val();
+
+}
+function writeStock(x){
+  database.ref('/').update({
+    Food:x
+  })
+}
